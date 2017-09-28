958b36475d73432d8eca06974f8ee0131685ef30
ymaomaoy <31032250+ymaomaoy@users.noreply.github.com>
2017-09-28 15:17:08 +0800

    Online Resume for IPND
    Resume Commit

---
+++ js/helper.js
@@ -0,0 +1,247 @@
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<h3 class="white-text">%data%</h3>';
+var HTMLheaderDescription = '<div class="selfIntroduction"></div>';
+var HTMLselfIntroduction = '<p>%data%</p>';
+var HTMLothers = '<p>%data%</p>';
+
+var HTMLbioPic = '<img src="%data%" alt="biopic" class="biopic">';
+
+var HTMLcontactGeneric = '<ul class="contact-entry"></ul>';
+var HTMLlocation = '<li class="location"><img src="images/icons/location.svg" class="svg" /><span class="bold-item">%data%</span></li>';
+var HTMLmobile = '<li class="mobile"><img src="images/icons/tel.svg" class="svg" /><span class="bold-item">%data%</span></li>';
+var HTMLemail = '<li class="email"><img src="images/icons/mail.svg" class="svg" /><span class="bold-item">%data%</span></li>';
+var HTMLlinkedin = '<li class="linkedin"><img src="images/icons/linkedin.svg" class="svg" /><span class="bold-item">%data%</span></li>';
+
+var HTMLworkStart = '<ul><li class="work-entry"></li></ul>';
+var HTMLworkTitle = '<h3 class="title-text">%data%</h3>';
+var HTMLworkEmployer = '<p class="employer-text">%data%</p>';
+var HTMLworkDates = '<p class="date-text">%data%</p>';
+var HTMLcompanyIntro = '<p class="companyIntro-text">%data%</p>';
+var HTMLworkDescripition = '<p class="workDescripition-text">%data%</p>';
+var HTMLmainResponsibility = '<p class="mainResponsibility">%data%</p>';
+
+
+var HTMLschoolStart = '<ul><li class="education-entry"></li></ul>';
+var HTMLschoolName = '<h3 class="list">%data%</h3>';
+var HTMLschoolCollecge = '<p class="college-text">%data%</p>';
+var HTMLschoolDates = '<p class="date-text">%data%</p>';
+var HTMLschoolDegree = '<p class="degree-text">%data%</p>';
+var HTMLschoolMajor = '<span class="major-text">&nbsp; - %data%</span>';
+
+var HTMLonlineClasses = '<h4>Online Classes</h4>';
+var HTMLonlineStart = '<ul><li class="onlinecourse-entry"></li></ul>';
+var HTMLonlineTitle = '<a href="#" class="onlineCourses">%data%</a>';
+var HTMLonlineSchool = '<p class="school-text">%data%</p>';
+var HTMLonlineDates = '<p class="date-text">%data%</p>';
+
+var HTMLskillsMap = '<img src="%data%" alt="skillmap" class="map"/>';
+var HTMLprofessionStart = '<h3 id="profession">Professional Skills</h3><ul id="profession" class="skills"></ul>';
+var HTMLprofessionalSkills = '<li>%data%</li>';
+var HTMLcapabilityStart = '<h3 id="capability">Capabilities</h3><ul id="capability" class="skills"></ul>';
+var HTMLcapabilities = '<li>%data%</li>';
+
+var HTMLhonorStart = '<ul><li class="honor-entry"></li></ul>';
+var HTMLhonorlName = '<h3 class="list">%data%</h3>';
+var HTMLhonorDates = '<p class="date-text">%data%</p>';
+var HTMLhonorDescription = '<p>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<span>%data%</span>';
+var HTMLprojectImage = '<img src="%data%">';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+/*
+The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+*/
+$(document).ready(function() {
+  $('button').click(function() {
+    var $name = $('#name');
+    var iName = inName($name.text()) || function() {};
+    $name.html(iName);
+  });
+});
+
+var clickLocations = [];
+
+function logClicks(x, y) {
+  clickLocations.push({
+    x: x,
+    y: y
+  });
+  console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x, y)
+});
+
+
+
+/*
+This is the fun part. Here's where we generate the custom Google Map for the website.
+See the documentation below for more details.
+https://developers.google.com/maps/documentation/javascript/reference
+*/
+var map; // declares a global map variable
+
+
+/*
+Start here! initializeMap() is called when page is loaded.
+*/
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+
+  /*
+  For the map to be displayed, the googleMap var must be
+  appended to #mapDiv in resumeBuilder.js.
+  */
+  map = new google.maps.Map(document.querySelector('#mapDiv'), mapOptions);
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.contacts.location);
+
+    // iterates through school locations and appends each location to
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    education.schools.forEach(function(school) {
+      locations.push(school.location);
+    });
+
+    // iterates through work locations and appends each location to
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    workExperience.jobs.forEach(function(job) {
+      locations.push(job.location);
+    });
+
+    workExperience.internship.forEach(function(job) {
+      locations.push(job.location);
+    });
+
+    return locations;
+  };
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat(); // latitude from the place service
+    var lon = placeData.geometry.location.lng(); // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    // hmmmm, I wonder what this is about...
+    google.maps.event.addListener(marker, 'click', function() {
+      // your code goes here!
+      infowindow.open(map, marker);
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  };
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+    locations.forEach(function(place) {
+      // the search request object
+      var request = {
+        query: place
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    });
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+/*
+Uncomment the code below when you're ready to implement a Google Map!
+*/
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+  //Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,357 @@
+var bio = {
+  "name": "YIJING YIN",
+  "role": "User Experience Designer" + " | " + "Product Designer",
+  "contacts": {
+    "mobile": "15618168277",
+    "email": "muyyj@hotmail.com",
+    "github": "ymaomaoy",
+    "location": "Shanghai",
+    "linkedin": "yijing yin"
+  },
+  "welcomeMessage": "Hi,I'm an UX designer with passion to make ideas happen.",
+  "skills": ["Interaction Design", "User Research", "Protytyping"],
+  "biopic": "images/me.png"
+};
+
+bio.self_introduction = "With a mixed background in Industrial Design, Interaction Design and Design Research," +
+  "I‘m keen on understanding people, their motivations and contexts in order to inform and create experiences that could be benefitial to users." + "<br>" +
+  "For me,design is to make ideas happen and to make things better." + "<br>" +
+  "As a designer obsessed over details , I am always searching for opportunities to make it better." +
+  "I appreciate team collaboration and hold a positive view to communicate with cross-discipline team." + "<br>";
+
+bio.others = "<br>More about me：Gamification Practitioner / Emoji fan / Cat lover / Lurker on Bilibili";
+
+bio.display = function() {
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  var formattedSelfIntroduction = HTMLselfIntroduction.replace("%data%", bio.self_introduction);
+  var formattedOthers = HTMLothers.replace("%data%", bio.others);
+
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+
+  $('#bio').append(formattedName + formattedRole);
+  $('#biopic').append(formattedBioPic);
+  $('#bio').append(HTMLheaderDescription);
+  $('.selfIntroduction').append(formattedSelfIntroduction + formattedOthers);
+  $('#contact').append(HTMLcontactGeneric);
+  $('.contact-entry').append(formattedMobile + formattedEmail + formattedLinkedin);
+  $('#footerContacts').append(formattedMobile + formattedEmail + formattedLinkedin);
+};
+
+bio.display();
+
+var workExperience = {
+  "jobs": [
+    {
+      "title": "User Experience Designer",
+      "employer": "Archex",
+      "location": "Shanghai",
+      "dates": "Jul 2016 till now",
+      "companyIntro": "ArcheX is a food, digital and business innovation firm.<br>",
+      "workDescripition": "Yijing worked on research, design, and user testing, as well as client communications for various lean UX and design projects. The projects include software development , gamified system design, smart retail and media research etc.Main work content includes:<br>",
+      "mainResponsibility": "&middot Product design.Define product strategy, product features and product requirement documents.<br>" +
+      "&middot Interaction design.Design and prototype user flows and experience.Make low-fi to hi-fi prototype.Animate transition and micro interactions with Principle and After Effect.<br>" +
+      "&middot Research. Conduct user research and usability testing,make interview tools, organize workshops to test initial ideas with prototypes.<br>"
+    },
+    {
+      "title": "UI Designer / Research Assistent",
+      "employer": "Bits x Bites",
+      "location": "Shanghai",
+      "dates": "Sep 2015 - Jan 2016",
+      "companyIntro": "Bits x Bites is China’s first purpose + profit, food-focused, accelerator venture capital platform.<br>",
+      "workDescripition": "Yijing worked for website redesign project as principal designer and service prototyping project as research assistant.Main work content includes:<br>",
+      "mainResponsibility": "&middot Define content and design user interfaces.Work closely with front-end developer. <br>" +
+        "&middot Conduct user research with service design approach.<br>" +
+        "&middot Facilitate workshops to test initial ideas and prototypes.<br>"
+    }
+  ],
+  "internship": [
+    {
+      "title": "Interaction Design Intern",
+      "employer": "Tencent",
+      "location": "Shanghai",
+      "dates": "May - Sep 2015",
+      "companyIntro": "ISUX (Internet Social User Experience) ,Shanghai design center.<br>",
+      "workDescripition": "Worked on Qidian,Social-CRM management system,from early concepts, low-fi prototypes to hi-fi prototypes. Main work content includes:<br>",
+      "mainResponsibility": "&middot Define user flows and create wireframes.<br>" +
+        "&middot Define interaction specs.<br>"
+    },
+    {
+      "title": "Product Design Intern",
+      "employer": "Philips",
+      "location": "Shanghai",
+      "dates": "June - Aug 2014",
+      "companyIntro": "Philips Lighting, Shanghai design center.<br>",
+      "workDescripition": "Participated in several innovation projects .Main work content includes:<br>",
+      "mainResponsibility": "&middot Create storyboard, sketch ideas, make prototypes.<br>" +
+        "&middot Product design with 3D software.<br>" +
+        "&middot Conduct competitive research and desk research.<br>"
+    },
+    {
+      "title": "Design Intern",
+      "employer": "LOE Design",
+      "location": "Shanghai",
+      "dates": "Sep 2013 - Apr 2014",
+      "companyIntro": "LOE DESIGN is a professional design planning and design consulting company.<br>",
+      "workDescripition": "Participated in cross-discipline projects through design research, product design, motion design and visual design.",
+      "mainResponsibility": "Worked with design team in visual design exploration, storytelling and detailed interaction design."
+    }
+  ]
+};
+
+workExperience.display = function() {
+  for (var i = 0; i < workExperience.jobs.length; i++) {
+    var formattedTitle = HTMLworkTitle.replace("%data%", workExperience.jobs[i].title);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", workExperience.jobs[i].employer);
+    var formattedDates = HTMLworkDates.replace("%data%", workExperience.jobs[i].dates);
+    var formattedCompanyIntro = HTMLcompanyIntro.replace("%data%", workExperience.jobs[i].companyIntro);
+    var formattedWorkDescription = HTMLworkDescripition.replace("%data%", workExperience.jobs[i].workDescripition);
+    var formattedMainResponsibility = HTMLmainResponsibility.replace("%data%", workExperience.jobs[i].mainResponsibility);
+
+    $("#workExperience").append(HTMLworkStart);
+    $(".work-entry:last").append(formattedTitle);
+    $(".work-entry:last").append(formattedEmployer);
+    $(".work-entry:last").append(formattedDates);
+    $(".work-entry:last").append(formattedCompanyIntro);
+    $(".work-entry:last").append(formattedWorkDescription);
+    $(".work-entry:last").append(formattedMainResponsibility);
+  }
+
+  for (var k = 0; k < workExperience.internship.length; k++) {
+    var formattedTitle = HTMLworkTitle.replace("%data%", workExperience.internship[k].title);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", workExperience.internship[k].employer);
+    var formattedDates = HTMLworkDates.replace("%data%", workExperience.internship[k].dates);
+    var formattedCompanyIntro = HTMLcompanyIntro.replace("%data%", workExperience.internship[k].companyIntro);
+    var formattedWorkDescription = HTMLworkDescripition.replace("%data%", workExperience.internship[k].workDescripition);
+    var formattedMainResponsibility = HTMLmainResponsibility.replace("%data%", workExperience.internship[k].mainResponsibility);
+
+    $("#workExperience").append(HTMLworkStart);
+    $(".work-entry:last").append(formattedTitle);
+    $(".work-entry:last").append(formattedEmployer);
+    $(".work-entry:last").append(formattedDates);
+    $(".work-entry:last").append(formattedCompanyIntro);
+    $(".work-entry:last").append(formattedWorkDescription);
+    $(".work-entry:last").append(formattedMainResponsibility);
+  }
+};
+
+workExperience.display();
+
+var education = {
+  "schools": [
+    {
+      "name": "Tongji University",
+      "college": "College of Innovation and Design",
+      "location": "Shanghai",
+      "dates": "2013-2016",
+      "degree": "Master",
+      "major": "Design"
+    },
+    {
+      "name": "HFG Offenbach",
+      "college": "Product Department",
+      "location": "Offenbach",
+      "dates": "2014-2015",
+      "degree": "Exchange study",
+      "major": "Product Design"
+    },
+    {
+      "name": "Jiangnan University",
+      "college": "School of Design",
+      "location": "Wuxi",
+      "dates": "2009-2013",
+      "degree": "Bachelor",
+      "major": "Industrial Design"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Intro To Programming",
+      "school": "Udacity",
+      "dates": "May - Nov 2017",
+      "url": "https://www.udacity.com/",
+    },
+    {
+      "title": "Leading for Creativity",
+      "school": "IDEO U",
+      "dates": "Oct - Nov 2016",
+      "url": "https://www.ideou.com/",
+    },
+  ]
+};
+
+education.display = function() {
+  for (var i = 0; i < education.schools.length; i++) {
+    var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
+    var formattedCollege = HTMLschoolCollecge.replace("%data%", education.schools[i].college);
+    var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+    var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+    var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+
+    $("#education").append(HTMLschoolStart);
+    $(".education-entry:last").append(formattedName);
+    $(".education-entry:last").append(formattedCollege);
+    $(".education-entry:last").append(formattedDates);
+    $(".education-entry:last").append(formattedDegree);
+    $(".education-entry:last").append(formattedMajor);
+  }
+
+  if (education.onlineCourses.length > 0) {
+    $("#education").append(HTMLonlineClasses);
+    for (var k = 0; k < education.onlineCourses.length; k++) {
+      var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[k].title);
+      var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[k].school);
+      var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[k].dates);
+      var TitleLink = formattedTitle.replace("#", education.onlineCourses[k].url);
+
+      $("#education").append(HTMLonlineStart);
+      $(".onlinecourse-entry:last").append(TitleLink);
+      console.log(TitleLink);
+      $(".onlinecourse-entry:last").append(formattedSchool);
+      $(".onlinecourse-entry:last").append(formattedDates);
+    }
+  }
+};
+
+education.display();
+
+var projects = [
+  {
+    "title": "Engaging - Rewarding system",
+    "dates": "July 2016 - Apr 2017",
+    "description": "Rewarding system is a gamified application to improve user engagement of ERP application," +
+      " which helps salesmen to place order and record visits." +
+      " As a Web App on Wechat platform, users can easily access to rewarding system." +
+      " In rewarding system, users can get reward, exchange for trophy and edit their profiles.",
+    "images": ["images/enaging.png"]
+  },
+  {
+    "title": "Magenta Research",
+    "dates": "June - August 2017",
+    "description": "Media research on Huge,a design firm that has created its own media platform.",
+    "images": []
+  },
+  {
+    "title": "Lab000",
+    "dates": "June - August 2017",
+    "description": "Resrach for hot topic:\'new retail\'.",
+    "images": []
+  }
+];
+
+projects.display = function() {
+  if (projects.length > 0) {
+    for (var i = 0; i < projects.length; i++) {
+      var formattedTitle = HTMLprojectTitle.replace("%data%", projects[i].title);
+      var formattedDates = HTMLprojectDates.replace("%data%", projects[i].dates);
+      var formattedDescription = HTMLprojectDescription.replace("%data%", projects[i].description);
+
+      $('.projects').append(HTMLprojectStart);
+      $('.project-entry:last').append(formattedTitle);
+      $('.project-entry:last').append(formattedDates);
+      $('.project-entry:last').append(formattedDescription);
+    }
+
+    // for (var j = 0, jLen = projects[i].images.length; j < jLen; j++) {
+    //   var formattedImage = HTMLprojectImage.replace(DATA, projects.projects[i].images[j]);
+    //
+    //   $('.project-entry:last').append(formattedImage);
+    // }
+  }
+};
+projects.display();
+
+var skills = {
+  "skillpic": "images/skills.png",
+  "professional": [
+    "Skilled at design methods and design thinking, understanding of how to design through research, analysis, product insights and user-centered design approach.",
+    "Basic knowledge in HTML / CSS / Javascript / Python,as well as agile methodologies."
+  ],
+  "capabilities": [
+    "Proficient in spoken and written English and Chinese, basic knowledge in German.",
+    "Ability of communication, collaboration and observation."
+  ]
+};
+
+skills.display = function() {
+  var formattedSkillMap = HTMLskillsMap.replace("%data%", skills.skillpic);
+  $("#skills").append(formattedSkillMap);
+  $("#skills").append(HTMLprofessionStart);
+  $("#skills").append(HTMLcapabilityStart);
+
+  for (var i = 0; i < skills.professional.length; i++) {
+    var formattedProfession = HTMLprofessionalSkills.replace("%data%", skills.professional[i]);
+    $("#profession.skills:last").append(formattedProfession);
+  }
+
+  for (var k = 0; k < skills.capabilities.length; k++) {
+    var formattedCapability = HTMLcapabilities.replace("%data%", skills.capabilities[k]);
+    $("#capability.skills:last").append(formattedCapability);
+  }
+};
+
+skills.display();
+
+var honor = {
+  "certifications": [
+    {
+      "name": "Leading for Creativity",
+      "dates": "Nov 2016",
+      "descripition": "IDEO U,online training program"
+    },
+    {
+      "name": "Intro To Programming",
+      "dates": "Oct 2017",
+      "descripition": "Udacity,online training program"
+    }
+  ],
+  "awards": [
+    {
+      "name": "Merit Award",
+      "dates": "Aug 2014",
+      "descripition": "Lite-on Award"
+    },
+    {
+      "name": "Best Promotion Award &" + "<br>Best Environment friendly Award",
+      "dates": "Apr 2014",
+      "descripition": "International Higher Education Collaboration on Upcycling of Industry"
+    },
+    {
+      "name": "Best Graduation Design",
+      "dates": "Jun 2013",
+      "descripition": "School of design,Jiangnan University"
+    }
+  ]
+};
+
+honor.display = function() {
+  for (var i = 0; i < honor.certifications.length; i++) {
+    var formattedName = HTMLhonorlName.replace("%data%", honor.certifications[i].name);
+    var formattedDates = HTMLhonorDates.replace("%data%", honor.certifications[i].dates);
+    var formattedDescription = HTMLhonorDescription.replace("%data%", honor.certifications[i].descripition);
+
+    $("#honor").append(HTMLhonorStart);
+    $(".honor-entry:last").append(formattedName);
+    $(".honor-entry:last").append(formattedDates);
+    $(".honor-entry:last").append(formattedDescription);
+  }
+
+  for (var k = 0; k < honor.awards.length; k++) {
+    var formattedName = HTMLhonorlName.replace("%data%", honor.awards[k].name);
+    var formattedDates = HTMLhonorDates.replace("%data%", honor.awards[k].dates);
+    var formattedDescription = HTMLhonorDescription.replace("%data%", honor.awards[k].descripition);
+
+    $("#honor").append(HTMLhonorStart);
+    $(".honor-entry:last").append(formattedName);
+    $(".honor-entry:last").append(formattedDates);
+    $(".honor-entry:last").append(formattedDescription);
+  }
+};
+
+honor.display();
+
+$('#mapDiv').append(googleMap);
